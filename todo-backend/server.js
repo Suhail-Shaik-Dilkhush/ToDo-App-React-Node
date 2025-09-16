f23551f04d47df0382e9821c3143d4bb9d889fc7
suhail <dilkushsuhail6786@gmail.com>
2025-09-16 23:33:28 +0530

    created start.js and added the express listening on port 3000 to it

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const connectDB = require('./db');
+
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
-let port = 3000;
 
 
-app.get('/', (req, res) => {
+connectDB();
 
-    res.status(200).json({ message: 'This is Suhail - from the server' });
-
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+module.exports = app;
